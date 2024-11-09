a28707f1d721c3c3a5e4c43e4c29f68fef57e7d5
00yhsp <00yhsp@naver.com>
2024-11-09 14:21:27 +0200

    Initial Commit

---
+++ INJAE/INJAE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  INJAE
+//
+//  Created by 박현수 on 11/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ INJAE/INJAE/INJAEApp.swift
@@ -0,0 +1,17 @@
+//
+//  INJAEApp.swift
+//  INJAE
+//
+//  Created by 박현수 on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct INJAEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
