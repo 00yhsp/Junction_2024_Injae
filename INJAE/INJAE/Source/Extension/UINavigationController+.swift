5ef39958a6469c176536073a2079fc9c10f8b485
00yhsp <00yhsp@naver.com>
2024-11-10 08:16:14 +0200

    네비게이션 제스처 추가

---
+++ INJAE/INJAE/Source/Extension/UINavigationController+.swift
@@ -0,0 +1,20 @@
+//
+//  UINavigationController+.swift
+//  INJAE
+//
+//  Created by 박현수 on 11/10/24.
+//
+
+
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
